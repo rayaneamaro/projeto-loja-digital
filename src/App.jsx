d677d50a24a300de027f11d280310fe67d0227c8
Rayane Amaro <rayaneamaro96@gmail.com>
2024-12-01 21:44:26 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,25 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/produtos" element={<ProductListingPage />} />
       </Routes>
+    </Router>// src/App.jsx
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductViewPage from './pages/ProductViewPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/product/:id" element={<ProductViewPage />} />
+        {/* Outras rotas aqui */}
+      </Routes>
     </Router>
+  );
+}
+
+export default App;
+
   );
 };
 
