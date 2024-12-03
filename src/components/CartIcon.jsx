8d893c337ac5f5a7ba9542d0e182836e4e484ea4
Rayane Amaro <rayaneamaro96@gmail.com>
2024-12-03 04:16:28 -0300

    Update src directory

--- src/components/CartIcon.jsx
+++
@@ -1,5 +0,0 @@
-const CartIcon = () => {
-  return <img src="/assets/mini-cart.svg" alt="Carrinho" />;
-};
-
-export default CartIcon;
--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LogoImage from '../assets/logo-header.svg'; // Importando diretamente da pasta assets
-
-const Logo = () => {
-  return <img src={LogoImage} alt="Logo" width="253" height="44" />;
-};
-
-export default Logo;
